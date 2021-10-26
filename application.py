492142f4f6a306bbd6ab8e4e66d7ff00be7ef5ef
neilbox1 <neilgonzalez22@csu.fullerton.edu>
2021-10-25 20:36:57 -0700

    register page, dark theme, navbar tweaks

--- application.py
+++ application.py
@@ -3,14 +3,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    if request.args.get('emailuser'):
-         return render_template('home.html', userName=request.args.get('emailuser'))
+    if request.args.get('user'):
+         return render_template('home.html', userName=request.args.get('user'))
     return render_template('login.html')
   
 
 @app.route('/home', methods=['GET'])
 def home():
     return render_template('home.html')
+
+@app.route('/register', methods=['GET'] )
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
    app.run()
