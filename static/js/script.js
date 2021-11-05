f14999a9c832a69f0291eddf513badd3617faf84
Cruuise <ed.cruz198@gmail.com>
2021-11-04 18:59:12 -0700

    Details uses second API now

--- static/js/script.js
+++ static/js/script.js
@@ -42,18 +42,26 @@ function showMovie(data) {
         main.appendChild(movieEL);
     })
 }
-
 function movieSelected(id2){
-    sessionStorage.setItem('movieId',id2);
-    window.location = 'detail.html';
+    axios.get('https://api.themoviedb.org/3/movie/' +id2 +'/external_ids?' + API_KEY)
+     .then((response) => {
+         console.log(response);
+
+
+        result = response.data.imdb_id;
+        console.log(result);
+
+     })
+    sessionStorage.setItem('movieId',result);
+    window.location = 'detail';
     return false;
 }
 
 
 function getDetails(){
-    
     let movieId = sessionStorage.getItem('movieId');
-    axios.get('https://api.themoviedb.org/3/movie/' +movieId +'?' + API_KEY)
+
+    axios.get('http://www.omdbapi.com/?i='+movieId + '&apikey=9be27fce')
      .then((response) => {
          console.log(response);
         let movie = response.data;
@@ -63,22 +71,23 @@ function getDetails(){
             <div class="detail">
                 <div class="col-md-4">
                 </div>
-                    <img src="${IMG_URL+movie.poster_path}" class="image">
+                    <img src="${movie.Poster}" class="thumbnail">
                 <div class="col-md-8">
-                    <h2>${movie.title}</h2>
+                    <h2>${movie.Title}</h2>
                     <ul class="list-group">
-                        <li class="list-group-item"><strong>Title:</strong> ${movie.title}</li>
-                        <li class="list-group-item"><strong>Released:</strong> ${movie.release_date}</li>
-                        <li class="list-group-item"><strong>Status:</strong> ${movie.status}</li>
-                        <li class="list-group-item"><strong>Score:</strong> ${movie.vote_average}</li>
+                        <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+                        <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+                        <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+                        <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+                        <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
                     </ul>
                 </div>
             </div>
             <div class="detail">
                 <h3>Plot</h3>
-                ${movie.overview}
+                ${movie.Plot}
                 <hr>
-                <a href="home" target="_blank" class="btn btn-primary">Go Back</a>
+                <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
                 <a href="index.html" class="btn btn-default">Go Back To Search</a>
             </div>
         
